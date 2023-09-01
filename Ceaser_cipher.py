d8714fc628d784aecc38cb5638bf0e2eeb11da07
Anitor123 <anitorcletus@gmail.com>
2023-09-01 21:45:53 +0100

    feat:ceaser_cipher

---
+++ Ceaser_cipher.py
@@ -0,0 +1,7 @@
+start = "encode"
+end = "decode"
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+while start == "encode":
+    message = input("Type in the message: ")
+    message = message.lower().replace(" ", "")
+    
