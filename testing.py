7d848ecdbf1f33c7cf11cc1d6573a3a350a21a5a
Anitor123 <anitorcletus@gmail.com>
2023-09-10 22:40:17 +0100

    feat: Dicitionary freestyle

--- testing.py
+++ testing.py
@@ -9,3 +9,18 @@ home = {
 for p in home:
     print(p)
     print(home[p])
+
+
+the_boys ={
+    "Abraham":["Software Programming","Male","Tennis","24"],
+    "Godswill":["Graphic Designing", "Male","Football","22"],
+    "Joel":["Footballer","Male","Football","19"],
+    "Evan":["Game Development","Male","Football","16"],
+    "Nehemiah":["Primary School", "Male","Football",9],
+
+}    
+
+for d in the_boys:
+    print(f"~{d}")
+    for f in the_boys[d]:
+        print(f"{f}")
