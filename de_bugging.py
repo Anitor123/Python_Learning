1ac5303dc2217588e1d3594ee52b71bb31ea6847
Anitor123 <anitorcletus@gmail.com>
2023-09-12 23:01:03 +0100

    feat: Debugging stuffs

---
+++ de_bugging.py
@@ -0,0 +1,18 @@
+def my_function():
+    for i in range(1,20):
+        if i == 20:
+            print(" you go it")
+
+
+my_function()
+
+
+# Play Computer
+year = int(input("What's your year of birth: "))
+if year > 1980 and year <= 1994:
+    print("You are a millenial")
+elif year >1994:
+    print("You are a Gen Z")    
+
+
+    
